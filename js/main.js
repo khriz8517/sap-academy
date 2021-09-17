d9a810aa6f0517f9e814faf665b95bfe9b946769
Peter <khriz8517@gmail.com>
2021-09-17 15:40:16 -0500

    sap academy actualizacion

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-Vue.component('modal', {
-    props: ['total_points'],
+Vue.component("modal", {
+    props: ["total_points"],
     methods: {
-        toggleModal : function(){
-            this.$emit('toggle-modal');
-        }
+        toggleModal: function () {
+            this.$emit("toggle-modal");
+        },
     },
     computed: {
-        modal_title: function(){
-            return this.total_points > 0 ? 'Felicidades tienes '+this.total_points+' puntos' : 'Bienvenido a SAP academy';
-        }
+        modal_title: function () {
+            return this.total_points > 0
+                ? "Felicidades tienes " + this.total_points + " puntos"
+                : "Bienvenido a SAP academy";
+        },
     },
     template: `
         <div class="modal-mask">
@@ -21,84 +23,246 @@ Vue.component('modal', {
                     </div>
                 </div>
             </div>
-        </div>`
-})
+        </div>`,
+});
 
 var app = new Vue({
-    el: '#app',
-    data:{
+    el: "#app",
+    data: {
         searching: false,
-        search_value: '',
+        search_value: "",
         search_count: 0,
         showModal: false,
         welcome: {
-            head: 'Bienvenido a',
-            title: 'sap academy',
-            foot: 'Completa cursos y gana puntos.'
+            head: "Bienvenido a",
+            title: "sap academy",
+            foot: "Completa cursos y gana puntos.",
         },
         progressbar: 0,
         total_points: 0,
-        stages: [
-            { id: 1, number: 0, value: 0, estado: 1 },
-            { id: 2, number: 1, value: 50, estado: 1 },
-            { id: 3, number: 2, value: 100, estado: 1 },
-            { id: 4, number: 3, value: 150, estado: 1 },
-            { id: 5, number: 4, value: 200, estado: 0 },
-            { id: 6, number: 5, value: 300, estado: 0 },
-            { id: 7, number: 6, value: 400, estado: 0 },
-            { id: 8, number: 7, value: 600, estado: 0 },
-            { id: 9, number: 8, value: 800, estado: 0 },
-        ],
+        // stages: [
+        //     { id: 1, number: 0, value: 0, estado: 1 },
+        //     { id: 2, number: 1, value: 50, estado: 1 },
+        //     { id: 3, number: 2, value: 100, estado: 1 },
+        //     { id: 4, number: 3, value: 150, estado: 1 },
+        //     { id: 5, number: 4, value: 200, estado: 0 },
+        //     { id: 6, number: 5, value: 250, estado: 0 },
+        //     { id: 7, number: 6, value: 300, estado: 0 },
+        //     { id: 8, number: 7, value: 350, estado: 0 },
+        //     { id: 9, number: 8, value: 400, estado: 0 },
+        // ],
         selected_category_id: 0,
-        selected_category_text: '',
+        selected_category_text: "",
         categorias: [
-            { id: 1, cat_short: 'Módulo SD', cat_large: 'Ventas y distribución', estado: 1 },
-            { id: 2, cat_short: 'Módulo TM', cat_large: 'Gestión de transporte', estado: 1 },
-            { id: 3, cat_short: 'Módulo EWM', cat_large: 'Gestión de almacenes', estado: 1 },
-            { id: 4, cat_short: 'Módulo CO', cat_large: 'Contabilidad de costos', estado: 1 },
-            { id: 5, cat_short: 'Módulo QM', cat_large: 'Calidad', estado: 1 },
-            { id: 6, cat_short: 'Módulo PM', cat_large: 'Mantenimiento', estado: 1 },
-            { id: 7, cat_short: 'Módulo PP', cat_large: 'Gestión de producción', estado: 0 },
-            { id: 8, cat_short: 'Módulo FI', cat_large: 'Finanzas', estado: 0 },
-            { id: 9, cat_short: 'Módulo MM', cat_large: 'Gestión de materiales', estado: 0}
+            {
+                id: 1,
+                cat_short: "Módulo SD",
+                cat_large: "Ventas y distribución",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 2,
+                cat_short: "Módulo TM",
+                cat_large: "Gestión de transporte",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 3,
+                cat_short: "Módulo EWM",
+                cat_large: "Gestión de almacenes",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 4,
+                cat_short: "Módulo CO",
+                cat_large: "Contabilidad de costos",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 5,
+                cat_short: "Módulo QM",
+                cat_large: "Calidad",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 6,
+                cat_short: "Módulo PM",
+                cat_large: "Mantenimiento",
+                progress: 0,
+                estado: 1,
+            },
+            {
+                id: 7,
+                cat_short: "Módulo PP",
+                cat_large: "Gestión de producción",
+                progress: 0,
+                estado: 0,
+            },
+            {
+                id: 8,
+                cat_short: "Módulo FI",
+                cat_large: "Finanzas",
+                progress: 0,
+                estado: 0,
+            },
+            {
+                id: 9,
+                cat_short: "Módulo MM",
+                cat_large: "Gestión de materiales",
+                progress: 0,
+                estado: 0,
+            },
         ],
         cursos_pool: [
-            { id: 1, cat_id: 1, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 70 },
-            { id: 2, cat_id: 1, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 50 },
-            { id: 3, cat_id: 1, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 30 },
-            { id: 4, cat_id: 2, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 10 },
-            { id: 5, cat_id: 2, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 90 },
-            { id: 6, cat_id: 3, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 50 },
-            { id: 7, cat_id: 3, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 30 },
-            { id: 8, cat_id: 4, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 10 },
-            { id: 9, cat_id: 5, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 90 },
-            { id: 10, cat_id: 5, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 50 },
-            { id: 11, cat_id: 5, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 30 },
-            { id: 12, cat_id: 6, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 10 },
-            { id: 13, cat_id: 6, curso: 'Nombre del Curso', curso_img: './images/curso.jpg', link: '#', progress: 90 }
-        ]
+            {
+                id: 1,
+                cat_id: 1,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 70,
+            },
+            {
+                id: 2,
+                cat_id: 1,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 50,
+            },
+            {
+                id: 3,
+                cat_id: 1,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 30,
+            },
+            {
+                id: 4,
+                cat_id: 2,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 10,
+            },
+            {
+                id: 5,
+                cat_id: 2,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 90,
+            },
+            {
+                id: 6,
+                cat_id: 3,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 50,
+            },
+            {
+                id: 7,
+                cat_id: 3,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 30,
+            },
+            {
+                id: 8,
+                cat_id: 4,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 10,
+            },
+            {
+                id: 9,
+                cat_id: 5,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 90,
+            },
+            {
+                id: 10,
+                cat_id: 5,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 50,
+            },
+            {
+                id: 11,
+                cat_id: 5,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 30,
+            },
+            {
+                id: 12,
+                cat_id: 6,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 10,
+            },
+            {
+                id: 13,
+                cat_id: 6,
+                curso: "Nombre del Curso",
+                curso_img: "./images/curso.jpg",
+                link: "#",
+                progress: 90,
+            },
+        ],
     },
-    created(){
+    created() {
         this.getProgressBarPercentage();
         this.getTotalPoints();
     },
     methods: {
-        getProgressBarPercentage: function(){
+        getProgressBarPercentage: function () {
             this.progressbar = 30;
         },
-        getTotalPoints: function(){
+        getTotalPoints: function () {
             this.total_points = 0;
         },
-        toggleModal : function(){
+        toggleModal: function () {
             this.showModal = !this.showModal;
-        }
+        },
     },
     computed: {
-        cursos: function (){
-            return this.cursos_pool.filter((item) => item.cat_id === this.selected_category_id);
+        stages: function () {
+            var array = [{ number: 0, value: 0, estado: 1 }];
+            var stage_points = 0;
+            for (let i = 0; i < this.categorias.length; i++) {
+                const element = this.categorias[i];
+                array.push({
+                    number: i + 1,
+                    value: (stage_points += 50),
+                    estado: element.progress == 100 ? 1 : 0,
+                });
+            }
+            return array;
         },
-        cursos_search: function (){
-            return this.cursos_pool.filter((item) => item.curso.toLowerCase().includes(this.search_value));
-        }
-    }
-})
+        cursos: function () {
+            return this.cursos_pool.filter(
+                (item) => item.cat_id === this.selected_category_id
+            );
+        },
+        cursos_search: function () {
+            return this.cursos_pool.filter((item) =>
+                item.curso.toLowerCase().includes(this.search_value)
+            );
+        },
+    },
+});
---
+++ js/presentacion.js
@@ -0,0 +1,70 @@
+Vue.component("modal", {
+    props: ["procesos_item"],
+    methods: {
+        toggleModal: function () {
+            this.$emit("toggle-modal");
+        },
+    },
+    template: `
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container" style="height: 35em">
+                    <div @click="toggleModal" class="modal-close"><span class="material-icons">cancel</span></div>
+                    <object :data="procesos_item.link_file" type="application/pdf" width="100%" height="100%">
+                        <p>En caso no se visualice el PDF has click <a :href="procesos_item.link_file">aqui!</a></p>
+                    </object>
+                </div>
+            </div>
+        </div>`,
+});
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        showModal: false,
+        procesos_item: {},
+        procesos: [
+            {
+                proceso_title: "Nombre del primer archivo adjunto",
+                updated: "Actualizado el 16 de junio de 2021",
+                material_icon: "picture_as_pdf",
+                link_file: "http://www.africau.edu/images/default/sample.pdf",
+                format: ".pdf",
+            },
+            {
+                proceso_title: "Nombre del primer archivo adjunto",
+                updated: "Actualizado el 16 de junio de 2021",
+                material_icon: "picture_as_pdf",
+                link_file: "http://www.africau.edu/images/default/sample.pdf",
+                format: ".pdf",
+            },
+            {
+                proceso_title: "Nombre del primer archivo adjunto",
+                updated: "Actualizado el 16 de junio de 2021",
+                material_icon: "picture_as_pdf",
+                link_file: "http://www.africau.edu/images/default/sample.pdf",
+                format: ".pdf",
+            },
+            {
+                proceso_title: "Nombre del primer archivo adjunto",
+                updated: "Actualizado el 16 de junio de 2021",
+                material_icon: "picture_as_pdf",
+                link_file: "http://www.africau.edu/images/default/sample.pdf",
+                format: ".pdf",
+            },
+        ],
+    },
+    created() {},
+    methods: {
+        toggleModal: function () {
+            this.showModal = !this.showModal;
+        },
+        scrollToElement: function (options) {
+            const el = this.$el.getElementsByClassName("index-50")[0];
+
+            if (el) {
+                el.scrollIntoView(options);
+            }
+        },
+    },
+});
